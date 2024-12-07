25ebeff1d248092e5a8443c29c3622bce4fef2c9
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2024-12-07 13:03:26 +0530

    Changes made again to .git on local

--- FullStack JWT Flow/Backend/DB/Users.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-    username:{type:String, unique:true, require:true},
-    password:{type:String, require:true},
-    refreshToken:{type:String}
-})
-
-export const User = mongoose.model('User',userSchema)
